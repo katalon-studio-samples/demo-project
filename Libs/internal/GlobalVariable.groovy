ea1de7346e735ceb46573d30f9703a3c6ee3a288
Brian Ducson <brian.ducson@gmail.com>
2019-03-12 11:23:37 +0700

    init project

---
+++ Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,42 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p></p>
+     */
+    public static Object username
+     
+    /**
+     * <p></p>
+     */
+    public static Object password
+     
+    /**
+     * <p></p>
+     */
+    public static Object api
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += RunConfiguration.getOverridingParameters()
+    
+            username = selectedVariables['username']
+            password = selectedVariables['password']
+            api = selectedVariables['api']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
